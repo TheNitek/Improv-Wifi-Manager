0274df9535d24481a6143430cb870f1aa9a944c9
Nitek <nitek@blickt.es>
2024-05-10 19:50:22 +0200

    Make it work

--- src/demo.ino
+++ src/demo.ino
@@ -3,10 +3,25 @@
 #include <WiFiManager.h>
 #include <ImprovWiFiLibrary.h>
 
+WiFiManager wm;
+
 bool connectWifi(const char *ssid, const char *password) {
+  wm.stopConfigPortal();
+
   WiFi.persistent(true);
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password, 0, NULL, true);
   WiFi.persistent(false);
+
+  uint8_t count = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(DELAY_MS_WAIT_WIFI_CONNECTION);
+    if (count > MAX_ATTEMPTS_WIFI_CONNECTION)
+    {
+      WiFi.disconnect();
+      return false;
+    }
+    count++;
+  }
 
   return true;
 }
@@ -14,9 +29,8 @@ bool connectWifi(const char *ssid, const char *password) {
 void setup() {
   Serial.begin(115200);
 
-  WiFiManager wm;
   wm.setConfigPortalBlocking(false);
-  wm.erase(true);
+  //wm.erase(true);
 
   ImprovWiFi improvSerial(&Serial);
 
@@ -28,13 +42,14 @@ void setup() {
     while(!wm.process()) {
       improvSerial.handleSerial();
       if(improvSerial.isConnected()) {
-        wm.stopConfigPortal();
+        break;
       }
     }
   }
 
   Serial.println("Setup done");
 }
+
 
 void loop() {   
   Serial.println(WiFi.status());
